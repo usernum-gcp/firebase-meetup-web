53edab1635da2323c463eed0968cc29f1ebc8467
demo user <anfeldman@google.com>
2020-07-11 18:22:02 +0300

    token processing on trigger

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,14 @@
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const {Storage} = require('@google-cloud/storage');
 const { Logging } = require('@google-cloud/logging');
-const logging = new Logging({
-  projectId: process.env.GCLOUD_PROJECT,
-});
+const projectId = process.env.GCLOUD_PROJECT
+const tokens_collection = '/labs/12-07-lab/qwicklab_tokens'
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
@@ -27,7 +31,7 @@ exports.addLabTokenOnAuth = functions.auth.user().onCreate(async(user) => {
 
       const usersRef = await admin.firestore().collection('attendees');
       const tokensRef = await admin.firestore()
-          .collection('/labs/12-07-lab/qwicklab_tokens')
+          .collection(tokens_collection)
           .where("available", "==", true)
           .limit(1);
 
@@ -68,6 +72,58 @@ exports.addLabTokenOnAuth = functions.auth.user().onCreate(async(user) => {
 });
 
 
+exports.gcsFileSaved = functions.storage.object().onFinalize(async (object) => {
+  console.log('gcsFileSaved: event received')
+  try{
+    const fileBucket = object.bucket; // The Storage bucket that contains the file.
+    const filePath = object.name; // File path in the bucket.
+    const fileName = path.basename(filePath); // Get the file name.
+    const contentType = object.contentType; // File content type.
+
+    // Download file from bucket.
+    const bucket = admin.storage().bucket(fileBucket);
+    const tempFilePath = path.join(os.tmpdir(), fileName);
+    const metadata = {
+      contentType: contentType,
+    };
+    await bucket.file(filePath).download({destination: tempFilePath});
+    console.log('File %s downloaded locally to %s', fileName,  tempFilePath);
+
+    const parse = require('csv-parser');
+    let csv_parser = parse({delimiter: ','})
+    fs.createReadStream(tempFilePath)
+        .pipe(csv_parser)
+        .on('data',async  function(data){
+          try {
+            const token_id = data["Token id"];
+
+            const new_token = {
+              available: true,
+              token_id: token_id
+            };
+            // Add a new document in collection "cities" with ID 'LA'
+            const res = await admin.firestore().collection(tokens_collection).add(new_token);
+
+            console.log('createReadStream token_id %s added to collection %s with doc_id %s',
+                token_id,
+                tokens_collection,
+               res.id)
+          }
+          catch(err) {
+            console.log('gcsFileSaved:createReadStream: something went wrong:', err)
+          }
+        })
+        .on('end',function(){
+          console.log("createReadStream token_id saved")
+        });
+
+  }
+  catch (e) {
+    console.log('gcsFileSaved: something went wrong:', e)
+  }
+  console.log('gcsFileSaved end')
+  return
+});
 
 
 
